d8376d271ba9ac0963319f833474dc609b6f94c1
Erick-SN <ericksn0707@gmail.com>
2020-06-29 11:01:03 -0500

    Api request in order to get the artist info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
+import Song from './components/Song';
 import axios from 'axios';
 
 function App() {
   const [searchLyrics, setSearchLyrics] = useState({});
   const [lyrics, setLyrics] = useState('');
+  const [info, setInfo] = useState({});
   useEffect(() => {
     if (Object.keys(searchLyrics).length === 0) return;
     const getDataLyrics = async () => {
       const { artist, song } = searchLyrics;
       const URL = `https://api.lyrics.ovh/v1/${artist}/${song}`;
-      const response = await axios.get(URL);
-      setSearchLyrics(response.data.lyrics);
+      const URL_INFO = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+
+      const [lyrics, info] = await Promise.all([
+        axios.get(URL),
+        axios.get(URL_INFO),
+      ]);
+
+      setLyrics(lyrics.data.lyrics);
+      setInfo(info.data.artist[0]);
     };
     getDataLyrics();
   }, [searchLyrics]);
   return (
     <>
       <Form setSearchLyrics={setSearchLyrics} />
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-6'> </div>
+          <div className='col-md-6'>
+            <Song lyrics={lyrics} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Song.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Song = ({ lyrics }) => {
+  if (lyrics.length === 0) return null;
+  return (
+    <>
+      <h2>Song's lyrics</h2>
+      <p className='letra'>{lyrics}</p>
+    </>
+  );
+};
+
+export default Song;
