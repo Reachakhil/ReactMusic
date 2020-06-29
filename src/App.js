74fefbe5783d019ad3093bd1515d68ae09cb8dcd
Erick-SN <ericksn0707@gmail.com>
2020-06-29 10:46:04 -0500

    Api request in order to get the songs lyrics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
+import axios from 'axios';
 
 function App() {
+  const [searchLyrics, setSearchLyrics] = useState({});
+  const [lyrics, setLyrics] = useState('');
+  useEffect(() => {
+    if (Object.keys(searchLyrics).length === 0) return;
+    const getDataLyrics = async () => {
+      const { artist, song } = searchLyrics;
+      const URL = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+      const response = await axios.get(URL);
+      setSearchLyrics(response.data.lyrics);
+    };
+    getDataLyrics();
+  }, [searchLyrics]);
   return (
     <>
-      <Form />
+      <Form setSearchLyrics={setSearchLyrics} />
     </>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
-const Form = () => {
+import React, { useState } from 'react';
+const Form = ({ setSearchLyrics }) => {
+  const [search, setSearch] = useState({
+    artist: '',
+    song: '',
+  });
+  const [error, setError] = useState(false);
+  const { artist, song } = search;
+  const handleChange = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (artist.trim() === '' || song.trim() === '') {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setSearchLyrics(search);
+  };
   return (
     <>
       <div className='bg-info'>
+        {error ? (
+          <p className='alert alert-danger text-center p-2'>
+            Oops.. Something went wrong
+          </p>
+        ) : null}
         <div className='container'>
           <div className='row'>
-            <form className='col card text-white bg-transparent mb-5 pt-5 pb-2'>
+            <form
+              className='col card text-white bg-transparent mb-5 pt-5 pb-2'
+              onSubmit={onSubmit}>
               <fieldset>
                 <legend className='text-center'>React MusicLyrics</legend>
                 <div className='row'>
@@ -17,6 +45,8 @@ const Form = () => {
                         className='form-control'
                         name='artist'
                         placeholder='Artist'
+                        onChange={handleChange}
+                        value={artist}
                       />
                     </div>
                   </div>
@@ -28,14 +58,13 @@ const Form = () => {
                         className='form-control'
                         name='song'
                         placeholder='Song'
+                        onChange={handleChange}
+                        value={song}
                       />
                     </div>
                   </div>
                 </div>
-                <button
-                  type='submit'
-                  //   onClick={}
-                  className='btn btn-primary float-right'>
+                <button type='submit' className='btn btn-primary float-right'>
                   Search
                 </button>
               </fieldset>
